b086ed5458330895448a40cc56067ae6aaa21716
Scofield088 <oforu81@gmail.com>
2024-12-29 00:18:32 +0530

    Update LoginPage.jsx

--- orb1/src/pages/LoginPage.jsx
+++ orb1/src/pages/LoginPage.jsx
@@ -26,7 +26,6 @@ const LoginPage = () => {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(userData),
-          credentials: 'include',
         }
       );
 
